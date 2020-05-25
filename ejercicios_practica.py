6243ca96e2080c98280fb8035471cffc815d1cca
sonia134 <65562168+sonia134@users.noreply.github.com>
2020-05-24 22:42:31 -0300

    Add files via upload

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -31,6 +31,27 @@ def ej1():
       ej: La suma entre 4.2 y 6.5 es 10.7
 
     '''
+    print('Ingrese 1º numero a calcular:')
+    num_1  =  int(input())
+    print('Ingrese 2º numero a calcular:')
+    num_2  =  int(input())
+
+    suma  = num_1  +  num_2   # Operamos la suma
+    # Imprimimos en consola el resultado
+    print ( 'El resultado de la suma es' , suma )
+
+    resta  =  num_1  -  num_2  # Operamos la resta
+    print ( 'El resultado de la resta es' , resta )
+
+    mult  =  num_1  *  num_2  # Operamos la multiplicacion
+    print ( 'El resultado de la multiplicacion es' , mult )
+
+    div  =  num_1  /  num_2  # Operamos la division
+    print ( 'El resultado de la division8 es' , div )
+
+    exp= num_1 ^ num_2 # Operamos la potencia
+    print ('El resultado de la potencia es:', exp )
+
 
 def ej2():
     # Ejercicios de práctica numérica y cadenas
@@ -50,7 +71,27 @@ def ej2():
       está hablando.
 
     '''
+    print('Ingrese Nombre de la Persona:')
+    nombre = str(input())
+
+    print('Ingrese Apellido de la Persona:')
+    apellido = str(input())
+
+    print('Ingrese D.N.I.:')
+    dni = str(input())
+
+    print('Ingrese la Edad:')
+    edad = int(input())
+    
+    print('Ingrese Altura:')
+    altura = float(input())
+
+    print('El nombre completo es:', nombre + " " + apellido + " " + 'D.N.I.:', dni)
+    print('EL nombre y apellido ingresado corresponde a:', nombre, " ", apellido, 'D.N.I. es:', dni, " ", 'Edad:',edad, " ", 'Altura es:', altura)
+  
 def ej3():
+
+  
     # Ejercicios de práctica con cadenas
 
     '''
@@ -79,6 +120,28 @@ def ej3():
     Cualquier duda con el método split pueden consultarla por el campus
         
     '''
+    #caracter_inicial = pais_objetivo[0] 
+    #print(caracter_inicial)
+    print('Ingrese Nombre y Apellido completo del Padre:')
+    nom_padre= str(input())
+    nombre= nom_padre.split(" ")
+    ap_padre= nombre[-1]
+    print(ap_padre)
+
+    print('Ingrese Nombre y Apellido completo de la Madre:')
+    nom_madre= str(input())
+    nombre2= nom_madre.split(" ")
+    ap_madre= nombre2[-1]
+    print(ap_madre)
+
+    print('Ingrese el Nombre del Hijo:')
+    nom_hijo= str(input())
+    
+    print('El nombre completo del Hijo es:', nom_hijo, " ", ap_padre, " ", ap_madre)
+
+
+
+
 
 def ej4():
     # Ejercicios de práctica con cadenas
@@ -98,7 +161,6 @@ def ej4():
 
     NOTA: Para extraer el apellido del nombre recomendamos usar el método "split"
     Mostraremos un ejemplo:
-
     direccion_completa = 'Monroe 2716'
     calle, altura = direccion_completa.split(' ')
 
@@ -111,6 +173,22 @@ def ej4():
         
     '''
 
+    print('Ingrese Nombre y Apellido de la Primer Persona:')
+    nom_primera= str(input())
+    pri_pers= nom_primera.split(" ")
+    persona_1= pri_pers[-1]
+
+    print('Ingrese Nombre y Apellido de la Segunda Persona:')
+    nom_segunda= str(input())
+    seg_pers= nom_segunda.split(" ")
+    persona_2= seg_pers[-1]
+
+    es_pariente = persona_2 in persona_1   # es_direccion_argentina es un resultado tipo Bool (True,False)
+  
+    print(persona_2,'es un pariente?',es_pariente)
+   
+
+    
 def ej5():
     # Ejercicios de práctica con cadenas
        
@@ -137,11 +215,23 @@ def ej5():
     
 
     '''
+    print('ingrese su Nombre Completo')
+
+    nombre_min= str(input())
+
+    nombre_may= nombre_min.upper()
+    
+    prim_mayus= nombre_min.capitalize()
+    
+    print(nombre_min.lower())
+    print(nombre_may.upper())
+    print(prim_mayus)
+
 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
-    ej1()
+    #ej1()
     #ej2()
     #ej3()
-    #ej4()
+    ej4()
     #ej5()
